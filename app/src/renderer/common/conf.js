adbba60d4ff5a9e7d2c656ad52cf433836abb6ad
lpgo <whereisgirl@126.com>
2017-04-14 11:18:19 +0800

    conf

---
+++ app/src/renderer/common/conf.js
@@ -0,0 +1,3 @@
+export default {
+	server: "http://182.61.33.210",
+}
--- app/src/renderer/components/Login.vue
+++ app/src/renderer/components/Login.vue
@@ -2,22 +2,25 @@
 	<div class="login">
 		<mu-paper class="paper" :zDepth="4" >
 			<h2 class="center">系统登录</h2>
-			<mu-text-field label="用户名" hintText="请输入用户名" type="text" labelFloat fullWidth icon="account_box" v-model="name"/>
-			<mu-text-field label="密码" hintText="请输入密码" type="password" labelFloat fullWidth style="margin-bottom:30px" icon="vpn_key" v-model="pwd" :errorText="msg"/>
+			<mu-text-field label="用户名" hintText="请输入用户名" type="text" labelFloat fullWidth icon="account_box" v-model="name" :errorText="nameMsg" @change="nameMsg=null;pwdMsg=null"/>
+			<mu-text-field label="密码" hintText="请输入密码" type="password" labelFloat fullWidth style="margin-bottom:30px" icon="vpn_key" v-model="pwd" :errorText="pwdMsg" @change="nameMsg=null;pwdMsg=null" @keyup.enter="login"/>
 			<mu-raised-button label="登录" v-on:click="login" class="loginBtn" labelClass="loginText" primary />
 		</mu-paper>
  
 	</div>
 </template>
-
+ 
 <script>
+import conf from '../common/conf.js'
+
 export default {
 	name: "login",
 	data() {
 		return {
 			name:'',
 			pwd: '',
-			msg: null,
+			nameMsg: null,
+			pwdMsg: null,
 		};
 	},
 	methods: {
@@ -25,20 +28,22 @@ export default {
 			const form = new FormData();
 			form.append("name",this.name);
 			form.append("pwd", this.pwd);
-			fetch("http://182.61.33.210/login",
+			fetch(conf.server+"/login",
 	          	{
 		            credentials:"include",
 		            method: "POST",
 		            body:form,
 	          	}
-	        ).catch(err => console.log(err))
-	        .then(resp => resp.json())
-	        .then(result => {
-	        	if(result.ok) {
+	        ).catch(err => console.log("catch:", err))
+	        .then(res => {
+	        	if(res.ok) {
+	        		console.log(res);
 	        		this.$router.push("/purchase");
-	        		console.log(result.data);
 	        	} else {
-	        		this.msg = "用户名或密码错误";
+	        		res.json().then(result =>{
+	        			this.nameMsg = result.error;
+	        			this.pwdMsg = result.error;
+	        		});
 	        	}
 			});
 		}
