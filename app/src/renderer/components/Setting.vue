f9ca8772a6bec6ff334bcd56fe7ebe411849158a
lpgo <whereisgirl@126.com>
2017-04-20 17:00:49 +0800

    a

---
+++ app/src/renderer/components/Setting.vue
@@ -0,0 +1,42 @@
+<template>
+	<div style="padding:20px;font-size:16px;">
+		<span style="font-size:20px">型号：</span>
+		<template v-for="item,index in typeData">
+			<mu-chip class="demo-chip"  @delete="remove(index)" showDelete>{{item.name}}</mu-chip>
+		</template>
+		<mu-text-field  v-model="type" style="margin-left:20px;width:100px;"/><mu-raised-button label="添加" primary @click="add" style="margin:0 20px;"/>
+		<hr/>
+	</div>
+</template>
+
+<script>
+export default {
+	data() { 
+		return {
+			type:'',
+			typeData:[
+				{id:'1',name:"c20"},
+				{id:'2',name:"c30"},
+				{id:'3',name:"c40"},
+			],
+		};
+	},
+	methods: {
+		add() {
+			if(this.type) {
+				this.typeData.push({id:"1",name:this.type});
+			}
+			this.type = null;
+		},
+		remove(index) {
+			this.typeData.splice(index,1);
+		},
+	},
+}
+</script>
+
+<style>
+.demo-chip{
+  margin: 4px;
+}
+</style>
--- app/src/renderer/routes.js
+++ app/src/renderer/routes.js
@@ -4,6 +4,7 @@ import Test from 'components/Test'
 import Home from 'components/Home'
 import Purchase from 'components/Purchase'
 import Sale from 'components/Sale'
+import Setting from 'components/Setting'
 
 export default [
 	{
@@ -20,6 +21,7 @@ export default [
       	{ path: '/test', component: Test, name: '测试' },
         { path: '/sale', component: Sale, name: '销售' },
         { path: '/purchase', component: Purchase, name: '过磅单' },
+        { path: '/setting', component: Setting, name: '设置' },
       ]
     }
 ]
