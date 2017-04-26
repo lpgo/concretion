5c859c4796a3137be2b13abb7149beb4f060e2ab
lpgo <whereisgirl@126.com>
2017-04-26 15:54:42 +0800

    a

--- app/src/renderer/components/Statistics.vue
+++ app/src/renderer/components/Statistics.vue
@@ -1,12 +1,19 @@
 <template>
-	<div style="padding:20px">
-		<table border="1" bordercolor="black" cellspacing="0" cellpadding="5">
+	<div style="padding:20px;">
+		<mu-radio label="当日" name="group" nativeValue="day" v-model="value" @change="change"/> 
+	  	<mu-radio label="当月" name="group" nativeValue="month" v-model="value"  @change="change"/>
+	  	<mu-radio label="自定义" name="group" nativeValue="user" v-model="value" style="margin-right:100px" />  
+		<mu-date-picker mode="landscape" hintText="开始时间" v-model="start" :disabled="value!='user'"/>
+		<mu-date-picker mode="landscape" hintText="结束时间" v-model="end"  :disabled="value!='user'"/>
+		<mu-raised-button label="统计"  primary @click="statistics" :disabled="value!='user'"/>
+		<h2>销售统计</h2>
+		<table border="1" bordercolor="black" cellspacing="0" cellpadding="5" width="100%" >
 			<tr>  
 	            <td>施工单位</td>  
 	            <td>强度</td>  
-	            <td colspan="4" align="center">泵送</td>
-	            <td colspan="4" align="center">自卸</td>
-	            <td align="center">合计/元</td>  
+	            <td colspan="4">泵送</td>
+	            <td colspan="4">自卸</td>
+	            <td>合计/元</td>  
 	        </tr>
         	<template v-for="item in data">
         		<tr v-for="(value, key, index) in item">
@@ -42,6 +49,28 @@
 		        </tr> 
         	</template>
 		</table>
+		<h2>司机统计</h2>
+		<table border="1" bordercolor="black" cellspacing="0" cellpadding="5" width="100%" text-align="center">
+			<tr>  
+	            <td>司机</td>  
+	            <td>强度</td>  
+	            <td >方量</td>
+	            <td >方量总计</td>
+	            <td >次数</td>
+	            <td >次数总计</td>
+	        </tr>
+        	<template v-for="item in driverData">
+        		<tr v-for="value,index in item.items">
+    				<td :rowspan="item.size" v-if="index == 0">{{item.driver}}</td>  
+		            <td>{{value.strength}}</td>
+		            <td>{{value.capacity}}</td>
+		            <td :rowspan="item.size"  v-if="index == 0">{{item.cs}}</td>
+		            <td>{{value.count}}</td>
+		            <td :rowspan="item.size"  v-if="index == 0">{{item.total}}</td>
+		        </tr> 
+        	</template>
+		</table>
+
 	</div>
 </template>
 <script>
@@ -50,46 +79,103 @@ import moment from 'moment'
 export default {
 	data() {
 		return {
+			value:"day",
 			data:[],
+			driverData:[],
+			start:moment().format('YYYY-MM-DD'),
+			end:moment().format('YYYY-MM-DD'),
 		};
 	},
-	mounted() {
-		let start = encodeURIComponent(moment().startOf('month').format());
-		let end = encodeURIComponent(moment().endOf('month').format());
-		let url = `statistics?start=${start}&end=${end}`
-		util.get(url, data => {
-			//处理数据为表格准备
-			let map = new Map();
-			for(let item of data) {
-				if(map.has(item._id.com)) {
-					map.get(item._id.com).push(item);
-				} else {
-					map.set(item._id.com,[item]);
-				}
+	methods: {
+		statistics() {
+			this.get(this.start,this.end);
+		},
+		change(value) {
+			if('day' == value) {
+				this.start = moment().format('YYYY-MM-DD');
+				this.end = moment().format('YYYY-MM-DD');
+				this.get(this.start,this.end);
+			} else if('month' == value) {
+				this.start = moment().startOf('month').format('YYYY-MM-DD');
+				this.end = moment().endOf('month').format('YYYY-MM-DD');
+				this.get(this.start,this.end);
 			}
-			console.log(map);
-			let ss = [];
-			for(let [key, value] of map) {
-				let o = {};
-				let size = 0;
-				for(let item of value) {
-					if(o[item._id.strength]) {
-						o[item._id.strength][item._id.way] = {capacity:item.capacity,price:item._id.price,total:item.total};
-						o[item._id.strength].count += item.total;
+		},
+		get(start, end) {
+			let s = encodeURIComponent(moment(start).startOf('day').format());
+			let e = encodeURIComponent(moment(end).endOf('day').format());
+			let url = `statistics?start=${s}&end=${e}`
+			util.get(url, data => {
+				//处理数据为表格准备
+				let map = new Map();
+				for(let item of data) {
+					if(map.has(item._id.com)) {
+						map.get(item._id.com).push(item);
 					} else {
-						o[item._id.strength]={};
-						o[item._id.strength][item._id.way] = {capacity:item.capacity,price:item._id.price,total:item.total};
-						o[item._id.strength].count = item.total;
-						size++;
+						map.set(item._id.com,[item]);
 					}
+				} 
+
+				let ss = [];
+				for(let [key, value] of map) {
+					let o = {};
+					let size = 0;
+					for(let item of value) {
+						if(o[item._id.strength]) {
+							o[item._id.strength][item._id.way] = {capacity:item.capacity,price:item._id.price,total:item.total};
+							o[item._id.strength].count += item.total;
+						} else {
+							o[item._id.strength]={};
+							o[item._id.strength][item._id.way] = {capacity:item.capacity,price:item._id.price,total:item.total};
+							o[item._id.strength].count = item.total;
+							size++;
+						}
+					}
+					o.com = key;
+					o.size = size;
+					ss.push(o);	
 				}
-				o.com = key;
-				o.size = size;
-				ss.push(o);	
-			}
-			console.log(ss);
-			this.data = ss;
-		});
-	}
+				this.data = ss;
+			});
+
+			//根据司机统计
+			util.get(url+'&by=driver', data => {
+				//处理数据为表格准备
+				let map = new Map();
+				for(let item of data) {
+					if(map.has(item._id.driver)) {
+						map.get(item._id.driver).push(item);
+					} else {
+						map.set(item._id.driver,[item]);
+					}
+				} 
+				let ss = [];
+				for(let [key, value] of map) {
+					let o = {total:0,cs:0,size:0,items:[]};
+					for(let item of value) {
+						o.items.push({count:item.count,capacity:item.capacity,strength:item._id.strength});
+						o.total += item.count;
+						o.cs += item.capacity;
+						o.size++;
+					}
+					o.driver = key;
+					ss.push(o);	
+				}
+				this.driverData = ss;
+			});
+		},
+	},
+	mounted(){
+		this.get(this.start,this.end);
+	},
+	
 }
 </script>
+<style>
+	td {
+		text-align: center;
+	}
+	.radio {
+		padding-top:10px;
+	}
+</style>
