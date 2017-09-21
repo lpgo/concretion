829563c6defad8a783add6287cb060b8c2504754
lpgo <liupengko@gmail.com>
2017-09-21 21:04:10 +0800

    a

--- app/src/renderer/components/Home.vue
+++ app/src/renderer/components/Home.vue
@@ -206,22 +206,25 @@ export default {
     const web = remote.getCurrentWebContents();
     web.session.on('will-download', (e, item) =>{
           util.loading();
-          item.on('updated', () => {
-             console.log(item.getReceivedBytes());
-          });
-          item.on('done', (e, state) => {
-              if (state === 'interrupted') {
-                 alert("下载失败");
-              }
-              if (state === 'cancelled') {
-                  alert("下载取消");
-              }
-              if (state === 'completed') {
-                alert("导出完成");
-              } //下载完成，让 dock 上的下载目录Q弹一下下
-              console.log(state);
-              util.loaded();
-          });
+          try {
+            item.on('updated', () => {
+               console.log(item.getReceivedBytes());
+            });
+            item.on('done', (e, state) => {
+                if (state === 'interrupted') {
+                   alert("下载失败");
+                }
+                if (state === 'cancelled') {
+                    alert("下载取消");
+                }
+                if (state === 'completed') {
+                  alert("导出完成");
+                } //下载完成，让 dock 上的下载目录Q弹一下下
+                console.log(state);  
+            });
+          } finally {
+            util.loaded();
+          }
         });
   },
   components: {
